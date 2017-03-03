572cd08f3849ffb2e25aeea9b2449727acdddc2d
sbollers <sb@intervein.com>
2017-03-03 13:24:25 -0600

    initial commit

---
+++ COS-WebGalleryStart/app.js
@@ -0,0 +1,30 @@
+/*eslint-env node*/
+
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
+var app = express();
+
+// serve static files out of ./public
+app.use(express.static(__dirname + '/public'));
+
+// serve the initial view
+app.get('/', function (req, res) {
+  res.sendFile(__dirname +'/src/views/index.html');
+});
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
